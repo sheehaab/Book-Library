65376c7349bfed76968df673cb45b246eb7c44a1
sheehaab <shehabahmed963@gmail.com>
2020-11-03 00:26:01 +0200

    intializing the function and the constructor and the array that I save in the book objects that will be created by the user

---
+++ script/script.js
@@ -0,0 +1,12 @@
+//the array to store all the book object in it
+let myLibrary = [];
+
+//the constructor of the book objects
+function Book(){
+
+}
+
+//adding the books obj to the myLibrary array
+function addBookToLibrary(){
+
+}
